3fb289ad0f60bb22a633aa7b71c1c9ccba884284
Anton Patsev <patsev.anton@gmail.com>
2024-07-23 09:29:54 +0600

    Fix required_providers

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = ">= 0.72.0"  # Укажите актуальную версию
+      version = ">= 0.72.0"
     }
   }
   required_version = ">= 1.3"
